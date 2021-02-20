3fe347873f1cdabe0bcd17ee55ad8334ac4eec2a
NNT <nnt.nguyennhattruong@gmail.com>
2021-02-20 14:09:27 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Banner from './components/Banner/Banner';
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Details from './modules/details/Details';
+import Home from "./modules/Home/Home";
+import Cart from "./modules/Cart/Cart";
+import List from "./modules/list/List";
+import SearchResult from "./modules/SearchResult/SearchResult";
+import Payment from "./modules/Payment/Payment";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+          <Navbar/>
+          <Switch>
+              <Route exact path="/">
+                <Banner/>
+              </Route>
+            </Switch>
+          <div className="main">
+            <Switch>
+              <Route exact path="/">
+                <Home/>
+              </Route>
+              <Route path="/details/:id" render={({match})=>(
+                  <Details
+                      productId={match.params.id}
+                  />
+              )}>
+              </Route>
+              <Route path="/list/">
+                <List/>
+              </Route>
+              <Route path="/search/">
+                <SearchResult/>
+              </Route>
+              <Route path="/cart/">
+                <Cart/>
+              </Route>
+              <Route path="/payment">
+                <Payment/>
+              </Route>
+            </Switch>
+          </div>
+
+          <Footer/>
+        </div>
+    </Router>
+   
   );
 }
 
